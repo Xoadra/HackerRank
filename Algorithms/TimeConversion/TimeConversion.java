e12ec7c6f4f6989c33f622638cee8933a77c130b
Xoadra <xijp77@gmail.com>
2017-09-19 15:41:02 -0700

    Tested TimeConversion's original input against equals method changes.

--- Algorithms/TimeConversion/TimeConversion.java
+++ Algorithms/TimeConversion/TimeConversion.java
@@ -14,22 +14,22 @@ public class TimeConversion {
 
     static String timeConversion( String s ) {
         // Parse argument for key figures
-        int hours = Integer.parseInt( s.substring( 0, 2 ) );
+        Integer hours = Integer.parseInt( s.substring( 0, 2 ) );
         String minutes = s.substring( 3, 5 );
         String seconds = s.substring( 6, 8 );
         String meridiem = s.substring( 8, 10 );
         // Convert to military time
-        if ( hours != 12 && meridiem.equals( "PM" )  ) { hours += 12; }
-        if ( hours == 12 && meridiem.equals( "AM" ) ) { hours = 0; }
+        if ( !hours.equals( 12 ) && meridiem.equals( "PM" )  ) { hours += 12; }
+        if ( hours.equals( 12 ) && meridiem.equals( "AM" ) ) { hours = 0; }
         // Return statement
         return Integer.toString( hours ) + ":" + minutes + ":" + seconds;
     }
 
     public static void main( String[] args ) {
-		Scanner in = new Scanner( System.in );
-		
+		/* Scanner in = new Scanner( System.in ); */
 		String s = "12:40:22AM" /* in.next( ) */;
-
+		// "07:05:45PM"
+		// "12:40:22AM"
         String result = timeConversion( s );
         System.out.println( result );
     }
