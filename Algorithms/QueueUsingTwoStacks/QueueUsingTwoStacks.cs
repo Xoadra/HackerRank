3806adc32539c3c036dc6a59984c12c8fc221148
Xoadra <xijp77@gmail.com>
2017-09-23 01:18:49 -0700

    Starting code for the Queue Using Two Stacks algorithm.

---
+++ Algorithms/QueueUsingTwoStacks/QueueUsingTwoStacks.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+class QueueUsingTwoStacks {
+    
+	static void Main( String[] args ) {
+		Pile enq = new Pile( );
+		Pile deq = new Pile( );
+		//string s = Console.ReadLine( );
+		System.Console.WriteLine( "g" );
+	}
+	
+}
+
+
+class Node {
+    
+	// Attributes
+	public int data { get; set; }
+	public Node up { get; set; }
+
+	// Constructor
+	public Node( int data ) {
+		this.data = data;
+	}
+    
+}
+
+
+// Pseudo stack implemention
+class Pile {
+    
+	// Attributes
+	private Node top { get; set; } 
+
+	// Constructor
+	public Pile(  ) {
+		
+	}
+
+	// Methods
+	public void insert( int val ) {
+		Node node = new Node( val );
+	}
+
+	public Node delete(  ) {
+		return new Node( 6 );
+	}
+    
+}
+
+
+
